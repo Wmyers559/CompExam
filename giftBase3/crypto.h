47d855e88e4f5d5d6152d5690700ddf3242ef85f
Riley Myers <Wmyers559@users.noreply.github.com>
2019-07-03 12:55:59 -0600

    Removed stdio/stdlib funct, added default key

--- giftBase3/crypto.h
+++ giftBase3/crypto.h
@@ -11,6 +11,11 @@
 #pragma once
 #include <stdint.h>
 
+#define KEY_LENGTH 16
+#define DEFAULT_KEY {                               \
+    0x00, 0x11, 0x22, 0x44, 0x55, 0x66, 0x88, 0x99, \
+    0xaa, 0x01, 0x24, 0x56, 0x89, 0xa0, 0x12, 0x45}
+
 //----------------------------------
 // Function prototypes
 //----------------------------------
